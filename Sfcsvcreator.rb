ec131734c97169541cc9abc893caf779f50e36f5
Matthew Lim <125940571+MatthewTLim@users.noreply.github.com>
2024-08-05 19:54:52 -0700

    Add Spinner class and loading bar to terminal

--- Sfcsvcreator.rb
+++ Sfcsvcreator.rb
@@ -1,16 +1,26 @@
 require "httparty"
 require "nokogiri"
 require "csv"
+require "./Assets/spinner.rb"
 
 class Sfcsvcreator
 
   def create_csv(data)
+    puts ""
+    puts "Creating CSV..."
+
     csv_headers = ["company_name", "post_title", "job_category", "part_full_time", "location", "details", "job_post_date", "apply_link"]
+    spinner = Spinner.new(data.size)
 
     CSV.open("Silicone_forest_listing.csv", "wb", write_headers: true, headers: csv_headers, skip_blanks: true,) do |csv|
       data.each do |post|
         csv << post
+        spinner.spin
       end
     end
+
+
+    puts ""
+    puts "CSV created and saved sucessfully!"
   end
 end
