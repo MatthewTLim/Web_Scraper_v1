bdcdfe52048617d49b543330b28f98858e0669f4
Matthew Lim <125940571+MatthewTLim@users.noreply.github.com>
2024-09-09 20:58:18 -0700

    Add VCR to test

--- spec/linkscraper_spec.rb
+++ spec/linkscraper_spec.rb
@@ -6,9 +6,11 @@ RSpec.describe LinkScraper, :vcr do
   let(:scraper) { PageScraper.new }
   let(:link_scraper) { LinkScraper.new(base_url: "https://www.thesiliconforest.com/oregon-tech-jobs", driver: driver, wait: wait, scraper: scraper) }
 
-  it "retrieves job links" do
-    job_links = link_scraper.retrieve_links
-    expect(job_links).not_to be_empty
-    expect(job_links.size).to be > 1
+  it "retrieves job links", :vcr do
+    VCR.use_cassette("retrieves_job_links") do
+      job_links = link_scraper.retrieve_links
+      expect(job_links).not_to be_empty
+      expect(job_links.size).to be > 1
+    end
   end
 end
