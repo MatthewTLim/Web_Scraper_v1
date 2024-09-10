e0cf98c0931f477084ed7a6a0d6b222fcb4304a7
Matthew Lim <125940571+MatthewTLim@users.noreply.github.com>
2024-09-09 20:36:36 -0700

    Add retreive job link test

---
+++ spec/linkscraper_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../lib/scrapers/linkscraper'
+
+RSpec.describe LinkScraper, :vcr do
+  let(:driver) { Selenium::WebDriver.for(:chrome) }
+  let(:wait) { Selenium::WebDriver::Wait.new(timeout: 10) }
+  let(:scraper) { PageScraper.new }
+  let(:link_scraper) { LinkScraper.new(base_url: "https://www.thesiliconforest.com/oregon-tech-jobs", driver: driver, wait: wait, scraper: scraper) }
+
+  it "retrieves job links" do
+    job_links = link_scraper.retrieve_links
+    expect(job_links).not_to be_empty
+    expect(job_links.size).to be > 1
+  end
+end
