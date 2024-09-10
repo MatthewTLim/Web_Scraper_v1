2ee39cea4e35257c0efe54bc37c34a2eb99030ca
Matthew Lim <125940571+MatthewTLim@users.noreply.github.com>
2024-09-09 21:12:11 -0700

    Add mock_details

--- spec/support/mock_data.rb
+++ spec/support/mock_data.rb
@@ -4,3 +4,18 @@ def mock_csv_data
   path = File.join(File.dirname(__FILE__), '../../data/Silicone_forest_listing.csv')
   CSV.read(path, headers: true).first(10).map(&:to_h)
 end
+
+def mock_details
+  mock_csv_data.map do |row|
+    JobPostingPoro.new(
+      row["company_name"],
+      row["post_title"],
+      row["job_category"],
+      row["part_full_time"],
+      row["location"],
+      row["details"],
+      row["job_post_date"],
+      row["apply_link"]
+    )
+  end
+end
