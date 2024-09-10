f8340b9d2d2cea6c46b591bf630af235a87a417d
Matthew Lim <125940571+MatthewTLim@users.noreply.github.com>
2024-09-09 19:48:17 -0700

    Move jobporo to poro folder

--- Jobpostingporo.rb
+++
@@ -1,8 +0,0 @@
-JobPostingPoro = Struct.new(:company_name,
-                            :post_title,
-                            :job_category,
-                            :part_full_time,
-                            :location,
-                            :details,
-                            :job_post_date,
-                            :apply_link)
