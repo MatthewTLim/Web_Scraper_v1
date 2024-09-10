9e030504b0c3cb746aca488d4f7937ba5c03dab3
Matthew Lim <125940571+MatthewTLim@users.noreply.github.com>
2024-09-09 20:58:05 -0700

    Add cassette to test

--- spec/postscraper_spec.rb
+++ spec/postscraper_spec.rb
@@ -8,9 +8,11 @@ RSpec.describe PostScraper, :vcr do
   let(:post_processor) { PostProcessor.new }
   let(:post_scraper) { PostScraper.new(mock_links, http_client: http_client, parser: parser, post_processor: post_processor) }
 
-  it "scrapes job details from job links" do
-    job_posts = post_scraper.scrape_posts
-    expect(job_posts).not_to be_empty
-    expect(job_posts.size).to eq(10)
+  it "scrapes job details from job links", :vcr do
+    VCR.use_cassette("scrapes_job_details_from_job_links") do
+      job_posts = post_scraper.scrape_posts
+      expect(job_posts).not_to be_empty
+      expect(job_posts.size).to eq(10)
+    end
   end
 end
